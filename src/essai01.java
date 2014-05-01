d2078b66b889f764d977af3a17eb05beb68fc7d8
Jerome Moussalli <jerome.moussalli@gmail.com>
2014-05-01 21:55:00 +0200

    ...

--- src/essai01.java
+++ src/essai01.java
@@ -6,7 +6,6 @@ public class essai01 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
-		System.out.println("Bonjour tout le monde !");
 		System.out.println("Bonjour dana !");
 	}
 
